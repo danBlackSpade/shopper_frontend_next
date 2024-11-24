17b5d23e0c6d280aed12a14602b85410733696b8
Daniel Jun <djun44@gmail.com>
2024-11-24 14:17:15 -0300

    chore: remove comments

--- services/api/rides/rides.routes.ts
+++ services/api/rides/rides.routes.ts
@@ -13,22 +13,9 @@ export const getCustomerRides = async (userId: string, driverId?: string | null)
     if (driverId === 'all') {
       driverId = null;
     }
-    console.log('before endpoint', userId, driverId)
     const endpoint = driverId ? `/ride/${userId}/${driverId}` : `/ride/${userId}`;
     const response = await apiClient.get(endpoint);
 
-    console.log('after endpoint', response.data)
-    //check if error_code exists
-    console.log('Error not found', response.data.error_message)
-    
-    if (response.status == 404) {
-      console.log('%%% status response', response);
-      toast.error('response.data.error_message');
-      throw new Error(response.data.error_message);  
-    }
-    if (response.status !== 200) {
-      throw new Error('Erro ao obter os dados. Tente novamente. api1 ');
-    }
     toast.success('Dados carregados com sucesso. ');
     return response.data;
   } catch (err) {
